ec1a89a3603195f1de2a20ee232c08006315fcdf
Londrovski <jrmat49@gmail.com>
2026-04-16 06:26:40 +0100

    Add src/pages/ModelsPage.vue

---
+++ src/pages/ModelsPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-page class="q-pa-xl bg-grey-10 text-white" style="min-height: 100vh">
+    <div class="text-h4 q-mb-lg">3D Models</div>
+    <p class="text-grey-3">3D model viewers will go here.</p>
+  </q-page>
+</template>
