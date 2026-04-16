d96a6220d5f4c2adf4d13eafed075eb817af9da7
Londrovski <jrmat49@gmail.com>
2026-04-16 06:26:36 +0100

    Add src/pages/HomePage.vue

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="column items-center justify-center bg-grey-10 text-white" style="min-height: 100vh">
+    <div class="text-center q-pa-xl">
+      <div class="text-h2 text-weight-bold q-mb-sm">James Morris</div>
+      <div class="text-h6 text-grey-4 q-mb-xl">Design Technology</div>
+      <q-separator color="grey-7" class="q-mb-xl" />
+      <div class="row q-gutter-md justify-center">
+        <q-btn outline color="white" to="/projects" label="Projects" size="lg" />
+        <q-btn outline color="white" to="/sketchbooks" label="Sketchbooks" size="lg" />
+        <q-btn outline color="white" to="/models" label="3D Models" size="lg" />
+        <q-btn outline color="white" to="/about" label="About" size="lg" />
+      </div>
+    </div>
+  </q-page>
+</template>
