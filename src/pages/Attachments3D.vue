f3756719323fdbeedb681101d6f7e7fb08ba2005
Londrovski <jrmat49@gmail.com>
2026-04-16 07:04:49 +0100

    Build Attachments 3D page

---
+++ src/pages/Attachments3D.vue
@@ -0,0 +1,15 @@
+<template>
+  <STLViewer
+    title="Attachments"
+    :models="models"
+  />
+</template>
+
+<script setup>
+import STLViewer from 'components/STLViewer.vue'
+
+const models = [
+          { url: '/models/Handle.stl', color: 0x888888 },
+          { url: '/models/Handle-Cap-1.stl', color: 0x999999 }
+]
+</script>
