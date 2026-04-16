1fdd7f2dd8b35ec5716816e355b4b68cc1011a8e
Londrovski <jrmat49@gmail.com>
2026-04-16 06:26:39 +0100

    Add src/pages/SketchbooksPage.vue

---
+++ src/pages/SketchbooksPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-page class="q-pa-xl bg-grey-10 text-white" style="min-height: 100vh">
+    <div class="text-h4 q-mb-lg">Sketchbooks</div>
+    <p class="text-grey-3">Flipbook viewers will go here.</p>
+  </q-page>
+</template>
