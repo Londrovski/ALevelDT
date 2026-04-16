24b5a8d102f585abc84a756b117694ab36a60d9f
Londrovski <jrmat49@gmail.com>
2026-04-16 06:25:58 +0100

    Add quasar.config.js

---
+++ quasar.config.js
@@ -0,0 +1,19 @@
+const { configure } = require('quasar/wrappers')
+
+module.exports = configure(function (ctx) {
+  return {
+    boot: [],
+    css: ['app.scss'],
+    extras: ['material-icons'],
+    build: {
+      target: {
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+        node: 'node20'
+      },
+      vueRouterMode: 'hash'
+    },
+    devServer: { open: true },
+    framework: { config: {}, plugins: [] },
+    animations: []
+  }
+})
