d5bf06619b03c55ce517f9e44d6df014925e3f53
Londrovski <jrmat49@gmail.com>
2026-04-16 07:04:46 +0100

    Build The Full Monty 3D page

---
+++ src/pages/FullProject3D.vue
@@ -0,0 +1,14 @@
+<template>
+  <STLViewer
+    title="The Full Monty"
+    :models="models"
+  />
+</template>
+
+<script setup>
+import STLViewer from 'components/STLViewer.vue'
+
+const models = [
+          { url: '/models/The-Full-Monty-with-Top-2.stl', color: 0x888888 }
+]
+</script>
