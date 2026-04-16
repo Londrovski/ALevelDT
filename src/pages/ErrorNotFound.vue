7b07f41b0a94b5ea81061d82b8ecb224bf9b40a1
Londrovski <jrmat49@gmail.com>
2026-04-16 06:26:41 +0100

    Add src/pages/ErrorNotFound.vue

---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex flex-center bg-grey-10 text-white">
+    <div class="text-center">
+      <div class="text-h1">404</div>
+      <div class="text-h6 q-mb-md">Page not found</div>
+      <q-btn outline color="white" to="/" label="Go Home" />
+    </div>
+  </q-page>
+</template>
